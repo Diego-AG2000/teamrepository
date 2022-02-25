4a0bb00c29ca695b31d79873dd9a1a5a1c842a44
csimons637 <csimons637@gmail.com>
2022-02-25 15:21:59 -0500

    Worked on JumpCommand; having trouble with file access to jump.txt

--- CommandDesignPattern/JumpCommand.java
+++ CommandDesignPattern/JumpCommand.java
@@ -3,6 +3,42 @@
  */
 package CommandDesignPattern;
 
-public class JumpCommand {
-    
+import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.util.Scanner;
+
+public class JumpCommand implements Command {
+    public Player player;
+    File jump = new File("./jump.txt");
+    Scanner read = new Scanner(jump);
+
+    public JumpCommand(Player player) {
+        this.player = player;
+    }
+
+    public void execute() {
+        while(read.hasNextLine()) {
+            int i = 0;
+            for (i = 0; i < 6; i++) { // reads from zero to five
+                read.nextLine();
+            }
+            i = 0;
+            sleep(10);
+            clear();
+        }
+    }
+
+    private void sleep(int num) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(num);
+        } catch (Exception e) {
+            System.out.println("Timer error");
+        }
+    }
+
+    private void clear() {
+        System.out.println("\033[H\033[2]");
+    }
+
+
 }
