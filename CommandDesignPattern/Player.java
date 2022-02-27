517fa5f0f81fc5ce8e58ba680d1ecaa5acb313c5
csimons637 <csimons637@gmail.com>
2022-02-27 18:12:35 -0500

    Added player's methods

--- CommandDesignPattern/Player.java
+++ CommandDesignPattern/Player.java
@@ -1,5 +1,33 @@
+/**
+ * @author csimons637
+ */
 package CommandDesignPattern;
 
 public class Player {
     
+    /**
+     * Default constructor
+     */
+    public Player() {}
+    
+    /**
+     * Makes player jump
+     */
+    public void jump() {
+        JumpCommand.execute();
+    }
+
+    /**
+     * Makes player fire
+     */
+    public void fire() {
+        FireCommand.execute();
+    }
+
+    /**
+     * Makes player run
+     */
+    public void runForward() {
+        RunCommand.execute();
+    }
 }
