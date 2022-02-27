f52e60198b0c135d3d8cb9c55926cd3f827b1f53
DiegoG <diegoag@email.sc.edu>
2022-02-27 17:40:45 -0500

    Added fire command code

--- CommandDesignPattern/FireCommand.java
+++ CommandDesignPattern/FireCommand.java
@@ -4,6 +4,44 @@
  */
 package CommandDesignPattern;
 
-public class FireCommand {
+import java.util.concurrent.TimeUnit;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.Scanner;
+
+public class FireCommand implements Command{
+    public Player player;
+    File fire = new File("./fire.txt");
+    Scanner read = new Scanner(fire);
+
+    public FireCommand(Player player) {
+        this.player = player;
+    }
+
+    public void execute(){
+        while(read.hasNextLine()) {
+            int i = 0;
+            for(i = 0; i < 4; i++) {
+                System.out.println(read.nextLine());
+            }
+            i = 0;
+            sleep(1);
+        }
+        clear();
+    }
+
+    private void sleep(int num) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(num);
+        } catch (Exception e) {
+            System.out.println("Timmer error");
+        }
+    }
+    
+    private void clear() {
+        System.out.print("\033[H\033[2J");
+    }
     
 }
